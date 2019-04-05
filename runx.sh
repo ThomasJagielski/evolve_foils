d3f827edf883a55ab1d11f046d80b6199696e47f
Colin Snow <colinmsnow@gmail.com>
2019-04-04 20:47:18 -0400

    Edit comments and add .gitignore'

--- runx.sh
+++ runx.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Adapted from https://github.com/asurath/airfoil-analysis/blob/master/datanalysis.sh
+
 for file in ./sample16-2.dat
 do
 	#we write commands to a text file called xfoil.com, this first line overwrite the file and the subsequent lines append to it
+
 	echo "pane" > xfoil.com
 	echo "oper" >> xfoil.com
 	echo "visc 1e6" >> xfoil.com
 	echo "pacc" >> xfoil.com
-	#this is the only real dynamic line which we need
+
 	echo $file'.analysis' >> xfoil.com
 	echo "" >> xfoil.com
 	echo "alfa 0" >> xfoil.com
@@ -18,7 +21,7 @@ do
 	#we then run xfoil with its STDIN hooked up to the text file where we just wrote the commands to
 	xfoil $file < xfoil.com
 
-	#finally we delete the dat file as we have created the polar
+	#copy the results to a results.txt file and then delete the original file
     cp sample16-2.dat.analysis results.txt
     rm sample16-2.dat.analysis
 done
