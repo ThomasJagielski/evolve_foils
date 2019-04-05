b6a3656381b61292ca685609f4b2addce0bd2f59
Colin Snow <colinmsnow@gmail.com>
2019-04-04 20:27:58 -0400

    Clean up text files after execution to prevent errors in overwriting

--- evaluateFoil.py
+++ evaluateFoil.py
@@ -4,36 +4,38 @@ import sys
 import string
 import time
 import pandas as pd
-import runXfoil
 import os
 
-# file = open('sample16.dat',"r")
 
-# data = file.read()
-# file.close()
-data = pd.read_csv('sample16.dat')
-
-print(data)
-
-data.to_csv('sample16-2.dat', index=False)
+def edit_csv(file,operation):
+    data = pd.read_csv('sample16.dat')
 
 
+    data.to_csv('sample16-2.dat', index=False)
 
-sp.call(['./runx.sh'])
+
+def call_xfoil():
+
+    sp.call(['./runx.sh'])
 
 
 
-file = open('results.txt',"r")
+    file = open('results.txt',"r")
 
-data = file.read()
-file.close()
-data = data.split('-----')
+    data = file.read()
+    file.close()
+    open('results.txt', 'w').close()
+    data = data.split('-----')
 
-data = data[-1]
-data = data.split('  ')
+    data = data[-1]
+    data = data.split('  ')
 
-print(data[2], data[3])
+    return data[2], data[3]
 
+
+
+if __name__ == "__main__":
+    print(call_xfoil())
 
 # data = runXfoil.run_xfoil()
 
