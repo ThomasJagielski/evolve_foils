ff3660c8935356f288566c876c765c7817720ca0
thomasjagielski <tjagielski@olin.edu>
2019-04-27 17:42:38 -0400

    Add first draft of our evolutionary algorithm

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -32,7 +32,8 @@ def selection(population,num_indiv):
     sorted_population = sorted(population.items(), key=lambda v:v[1],reverse=True)
     return sorted_population[0:num_indiv]
 
-def EvoAlgo(num_population, operators, mu, lambda_, cxpb, mutpb, ngen):
+def EvoAlgo(num_population,num_indiv,top_indiv, num_cross,new_indiv_num,prob_mutate):
+    #operators, mu, lambda_, cxpb, mutpb, ngen):
     """
     Evolutionary algorithm that returns the most fit individual
         -- Follows the input format of eaMuPlusLambda algorithm in the DEAP package --
@@ -44,7 +45,30 @@ def EvoAlgo(num_population, operators, mu, lambda_, cxpb, mutpb, ngen):
     mu - number of individuals to be taken from nth generation to the (n+1)th generation
     lambda_ - number of offs
     """
+    population = create_pop(num_indiv)
+    for i in range(0,num_population):
+        selected = dict(selection(population,top_indiv))
+        most_fit_coeff = []
+        for key in selected:
+            most_fit_coeff.append(list(key))
+        population = selected
+        for _ in range(0,num_cross):
+            individual = TwoPointCrossover(most_fit_coeff[random.randint(0,top_indiv-1)],
+                                    most_fit_coeff[random.randint(0,top_indiv-1)])
+            for k in range(0,1):
+                population[tuple(individual[k])] = classes.evaluate_foil(individual[k])
+        
+        new_indiv = create_pop(new_indiv_num)
+        population.update(new_indiv)
+        #classes.mutate
+        print(i)
+        print(population)
+    
+
+    
 
 
 if __name__ == "__main__":
-    selected = dict(selection(create_pop(10),2))
+    #selected = dict(selection(create_pop(10),2))
+    #print(type(selected))
+    EvoAlgo(10,10,2,2,8,0.2)
