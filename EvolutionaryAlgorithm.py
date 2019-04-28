54c85c345689d3ddb92bb0a3bf4bf025067f51c9
thomasjagielski <tjagielski@olin.edu>
2019-04-28 19:23:39 -0400

    Add working version of the Evolutionary Algorithm

--- EvolutionaryAlgorithm.py
+++ EvolutionaryAlgorithm.py
@@ -32,7 +32,7 @@ def selection(population,num_indiv):
     sorted_population = sorted(population.items(), key=lambda v:v[1],reverse=True)
     return sorted_population[0:num_indiv]
 
-def EvoAlgo(num_population,num_indiv,top_indiv, num_cross,new_indiv_num,prob_mutate):
+def EvoAlgo(num_generations,num_indiv,top_indiv, num_cross,new_indiv_num,prob_add,prob_subtract):
     #operators, mu, lambda_, cxpb, mutpb, ngen):
     """
     Evolutionary algorithm that returns the most fit individual
@@ -46,23 +46,36 @@ def EvoAlgo(num_population,num_indiv,top_indiv, num_cross,new_indiv_num,prob_mut
     lambda_ - number of offs
     """
     population = create_pop(num_indiv)
-    for i in range(0,num_population):
+    for i in range(0,num_generations):
         selected = dict(selection(population,top_indiv))
         most_fit_coeff = []
         for key in selected:
             most_fit_coeff.append(list(key))
         population = selected
         for _ in range(0,num_cross):
-            individual = TwoPointCrossover(most_fit_coeff[random.randint(0,top_indiv-1)],
-                                    most_fit_coeff[random.randint(0,top_indiv-1)])
+            # Ensure the top airfoil remains the same from generation to generation
+            individual = TwoPointCrossover(most_fit_coeff[random.randint(1,top_indiv-1)],
+                                    most_fit_coeff[random.randint(1,top_indiv-1)])           
             for k in range(0,1):
                 population[tuple(individual[k])] = classes.evaluate_foil(individual[k])
-        
+        for key in population:
+            del population[key]
+            new_coeff = classes.mutate(list(key),prob_add,prob_subtract)
+            population[tuple(new_coeff[0])] = classes.evaluate_foil(new_coeff[0])
         new_indiv = create_pop(new_indiv_num)
         population.update(new_indiv)
+
+        # Run and save the best airfoil as the last one
+        selected = dict(selection(population,1))
+        # TODO Change to find better way to run key through evaluate_foil
+        for key in selected:
+            classes.evaluate_foil(key)
+            print(key,classes.evaluate_foil(key))
+
+
         #classes.mutate
         print(i)
-        print(population)
+        #print(population)
     
 
     
@@ -71,4 +84,4 @@ def EvoAlgo(num_population,num_indiv,top_indiv, num_cross,new_indiv_num,prob_mut
 if __name__ == "__main__":
     #selected = dict(selection(create_pop(10),2))
     #print(type(selected))
-    EvoAlgo(10,10,2,2,8,0.2)
+    EvoAlgo(10,10,2,2,8,0.2,0.8)
